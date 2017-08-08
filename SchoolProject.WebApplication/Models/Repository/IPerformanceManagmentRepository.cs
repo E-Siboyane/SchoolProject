3768fcde988b6cfefed3e8ce64aa0cdca4135825
Ngwako Malatji <Elvis.Malatji@gmail.com>
2017-08-08 09:28:08 +0200

    Added repository interactions

---
+++ SchoolProject.WebApplication/Models/Repository/IPerformanceManagmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.WebApplication.Models.Repository {
+   public interface IPerformanceManagmentRepository : IBaseRepository {
+        ApplicationDatabaseContext GetApplicationDbContext { get; }
+    }
+}
---
+++ SchoolProject.WebApplication/Models/Repository/PerformanceManagmentRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolProject.WebApplication.Models.Repository {
+    /// <summary>
+    /// Implementation of PerformanceManagement Repository, it inherits from the BaseRepository for common database CRUD Operations
+    /// </summary>
+    public class PerformanceManagmentRepository : BaseRepository, IPerformanceManagmentRepository, IDisposable {
+        private readonly ApplicationDatabaseContext _applicationDbContext;
+        public PerformanceManagmentRepository(ApplicationDatabaseContext _context) {
+            _applicationDbContext = _context;
+        }
+        /// <summary>
+        /// Return Application Database DbContext
+        /// </summary>
+        public ApplicationDatabaseContext GetApplicationDbContext { get { return _applicationDbContext; } }
+
+        /// <summary>
+        /// Dispose the Context after use
+        /// </summary>
+        public void Dispose() {
+            _applicationDbContext.Dispose();
+        }
+    }
+}
